38167ed61ecf04107d56d2584b80b319101f58b6
Meliosssss <phamhongquan19122001@gmail.com>
2023-05-30 18:59:18 +0700

    first commit

---
+++ src/main/java/fit/hutech/phamhongquan/MySqljpaApplication.java
@@ -0,0 +1,13 @@
+package fit.hutech.phamhongquan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqljpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySqljpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fit/hutech/phamhongquan/MySqljpaApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.hutech.phamhongquan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySqljpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
